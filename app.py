bb083085b9753aa7fdbdf08546c3ebd5b46c140b
Shivam bansal <98755494+Bansalshivam0@users.noreply.github.com>
2024-12-16 17:29:51 +0000

    AddedCSS and HTML files

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # print("Hello world !!")
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # print(__name__)
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # return "<p>Hello, World!</p>"
+    return render_template("home.html")
 
 if __name__ == "__main__" :
   app.run(host='0.0.0.0' , debug=True)
